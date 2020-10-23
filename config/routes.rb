3b297d4c067b1e753ec37bc389a659d0d9e90834
rumikoacopa <rumikorios@gmail.com>
2020-10-23 01:19:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+    #don't have to change any views because new and edit both use the same _form partial that already has the author_id
+  end
+  resources :posts
 end
